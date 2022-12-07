008d4ced1c697130c30e0ec7a6cd2dd26614d84d
dopezinho <opugnero@gmail.com>
2022-12-07 11:41:39 -0300

    erro

--- script.js
+++ script.js
@@ -47,7 +47,29 @@ class Produtinho {
     Salvar(product) {
         this.arrayProdutos.push(product)
         this.id++;
+    }
 
+    Listar() {
+        let tbody = document.getElementById('tbody')
+        tbody.innerText = ''
+
+        for(let i = 0; i < this.arrayProdutos.length; i++) {
+
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_nome = tr.insertCell();
+            let td_preco = tr.insertCell();
+            let td_del = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_nome.innerText = this.arrayProdutos[i].nomeProduto;
+            td_preco.innerText = this.arrayProdutos[i].precoProduto;
+            let imagem = document.createElement('img')
+            imagem.src = 'trash.png'
+            td_del.appendChild(imagem)            
+
+        }
     }
 }
 
